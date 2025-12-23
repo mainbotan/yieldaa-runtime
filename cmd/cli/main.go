b958338df43665e9fdc3b6e628cb51ceda795de0
mainbotan <ned.serafim@gmail.com>
2025-12-23 13:40:21 +0300

    fch

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	pkg, processed, fatalErrs := preset.LoadAndProcessPreset(
-		"./../../sources/presets/package5", 200)
+		"./../../sources/presets/package4", 200)
 
 	preset.PrintResults(pkg, processed, fatalErrs)
 
